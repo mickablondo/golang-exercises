9612779ab81188eb3e422afefa54e1385a39c20b
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2023-01-01 12:56:23 +0100

    #5

---
+++ src/easy/4-loops/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+/*
+ -- What do we learn ?
+ -- How to loop !
+ -- Hey mum, look at this carousel !
+*/
+func main() {
+	for i := 0; i < 3; i++ {
+		if i < 2 {
+			fmt.Println("Wooo ... looping ", i+1)
+		} else {
+			fmt.Println("Stooop ... i want to vomit ...")
+		}
+	}
+
+	fmt.Println("\n How do you write your name ?")
+	name := "MiK@"
+	for i, c := range name {
+		fmt.Println("Letter ", i, " is ", string(c))
+	}
+}
